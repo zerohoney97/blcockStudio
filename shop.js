ecab30ab19c907aa2d5a910efc58dede77d71b2b
1chori <pro4456@naver.com>
2023-03-20 17:34:30 +0900

    Done

--- shop.js
+++ shop.js
@@ -7,23 +7,63 @@ function Item(ATK,DEF,HP,MP) {
     this.MP = MP;
 }
 
-let sword = new Item(100,0,0,0);
-let shield = new Item(0,80,20,0);
-let drink = new Item(0,0,120,70);
+// weapon
 
-let weapon = document.querySelector('.normal .item_info');
-let armor = document.querySelector('.rare .item_info');
-let life = document.querySelector('.legend .item_info');
+let weapon_normal = new Item(10,0,0,0);
+let weapon_rare = new Item(30,0,0,0);
+let weapon_legend = new Item(50,0,0,0);
+
+let weapon_norm_info = document.querySelector('.weapon_normal .item_info');
+let weapon_rare_info = document.querySelector('.weapon_rare .item_info');
+let weapon_legend_info = document.querySelector('.weapon_legend .item_info');
+// 가격
+let price=0;
+
+weapon_norm_info.innerHTML = 'ATK : ' +weapon_normal.ATK+ '&nbsp&nbsp'+ 'DEF : ' +weapon_normal.DEF+'\n'+'<br>HP : ' +weapon_normal.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +weapon_normal.MP;
+weapon_rare_info.innerHTML = 'ATK : ' +weapon_rare.ATK+ '&nbsp&nbsp'+ 'DEF : ' +weapon_rare.DEF+'\n'+'<br>HP : ' +weapon_rare.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +weapon_rare.MP;
+weapon_legend_info.innerHTML = 'ATK : ' +weapon_legend.ATK+ '&nbsp&nbsp'+ 'DEF : ' +weapon_legend.DEF+'\n'+'<br>HP : ' +weapon_legend.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +weapon_legend.MP;
+
+// armor
+
+let armor_normal = new Item(0,20,0,0);
+let armor_rare = new Item(0,60,0,0);
+let armor_legend = new Item(0,100,0,0);
+
+let armor_norm_info = document.querySelector('.armor_normal .item_info');
+let armor_rare_info = document.querySelector('.armor_rare .item_info');
+let armor_legend_info = document.querySelector('.armor_legend .item_info');
 
 
+armor_norm_info.innerHTML = 'ATK : ' +armor_normal.ATK+ '&nbsp&nbsp'+ 'DEF : ' +armor_normal.DEF+'\n'+'<br>HP : ' +armor_normal.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +armor_normal.MP;
+armor_rare_info.innerHTML = 'ATK : ' +armor_rare.ATK+ '&nbsp&nbsp'+ 'DEF : ' +armor_rare.DEF+'\n'+'<br>HP : ' +armor_rare.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +armor_rare.MP;
+armor_legend_info.innerHTML = 'ATK : ' +armor_legend.ATK+ '&nbsp&nbsp'+ 'DEF : ' +armor_legend.DEF+'\n'+'<br>HP : ' +armor_legend.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +armor_legend.MP;
+
+// life
+
+let life_normal = new Item(0,0,60,0);
+let life_rare = new Item(0,0,110,0);
+let life_legend = new Item(0,0,170,0);
+
+let life_norm_info = document.querySelector('.life_normal .item_info');
+let life_rare_info = document.querySelector('.life_rare .item_info');
+let life_legend_info = document.querySelector('.life_legend .item_info');
 
 
-weapon.innerHTML = 'ATK : ' +sword.ATK+ '&nbsp&nbsp'+ 'DEF : ' +sword.DEF+'\n'+'<br>HP : ' +sword.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +sword.MP;
-armor.innerHTML = 'ATK : ' +shield.ATK+ '&nbsp&nbsp'+ 'DEF : ' +shield.DEF+'\n'+'<br>HP : ' +shield.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +shield.MP;
-life.innerHTML = 'ATK : ' +drink.ATK+ '&nbsp&nbsp'+ 'DEF : ' +drink.DEF+'\n'+'<br>HP : ' +drink.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +drink.MP;
+life_norm_info.innerHTML = 'ATK : ' +life_normal.ATK+ '&nbsp&nbsp'+ 'DEF : ' +life_normal.DEF+'\n'+'<br>HP : ' +life_normal.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +life_normal.MP;
+life_rare_info.innerHTML = 'ATK : ' +life_rare.ATK+ '&nbsp&nbsp'+ 'DEF : ' +life_rare.DEF+'\n'+'<br>HP : ' +life_rare.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +life_rare.MP;
+life_legend_info.innerHTML = 'ATK : ' +life_legend.ATK+ '&nbsp&nbsp'+ 'DEF : ' +life_legend.DEF+'\n'+'<br>HP : ' +life_legend.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +life_legend.MP;
 
 
 // 아이템 구매 클릭 시 발생하는 event
+
+let itemList=document.querySelectorAll('.item_price');
+console.log(itemList)
+
+for (let i = 0; i < itemList.length; i++) {
+itemList[i].addEventListener('click',()=>{
+    price=parseInt(itemList[i].innerText)  ;
+})    
+}
 
 function buy() {
     let popup = document.querySelector('.popup_background');
@@ -37,41 +77,85 @@ function buy() {
 // 돈이 부족할 때 발생하는 event
 
 let my_money = 1500;
-let item1 = 1000;
-let item2 = 500;
-let item3 = 400;
+
+let weapon_norm = 400;
+let weapon_rar = 700;
+let weapon_lege = 1100;
+
+let armor_norm = 300;
+let armor_rar = 600;
+let armor_lege = 1300;
+
+let life_norm = 300;
+let life_rar = 700;
+let life_lege = 1200;
+
+let weapon;
+let armor;
+let life;
 
 document.querySelector('.game_money').innerHTML=`<img src="./etherium.png"> ${my_money}`;
-document.querySelector('#item1').innerHTML = `<img src="./etherium.png">${item1}`;
-document.querySelector('#item2').innerHTML = `<img src="./etherium.png">${item2}`;
-document.querySelector('#item3').innerHTML = `<img src="./etherium.png">${item3}`;
+
+document.querySelector('#weapon_norm').innerHTML = `<img src="./etherium.png">${weapon_norm}`;
+document.querySelector('#weapon_rar').innerHTML = `<img src="./etherium.png">${weapon_rar}`;
+document.querySelector('#weapon_lege').innerHTML = `<img src="./etherium.png">${weapon_lege}`;
+
+document.querySelector('#armor_norm').innerHTML = `<img src="./etherium.png">${armor_norm}`;
+document.querySelector('#armor_rar').innerHTML = `<img src="./etherium.png">${armor_rar}`;
+document.querySelector('#armor_lege').innerHTML = `<img src="./etherium.png">${armor_lege}`;
+
+document.querySelector('#life_norm').innerHTML = `<img src="./etherium.png">${life_norm}`;
+document.querySelector('#life_rar').innerHTML = `<img src="./etherium.png">${life_rar}`;
+document.querySelector('#life_lege').innerHTML = `<img src="./etherium.png">${life_lege}`;
 
 
 // yes버튼 눌렀을 때 함수
 
     function onclickYes() {
         let popup = document.querySelector('.popup_background');
-        if (my_money - item1 >= 0) {
+
+        if (my_money  >= price) {
             popup.classList.remove('is_active');
-            my_money = my_money - item1;
-           
-        } else if(my_money - item2 >= 0){
-            popup.classList.remove('is_active');
-            my_money = my_money - item2;
-           
-        } else if(my_money - item3 >= 0){
-            popup.classList.remove('is_active');
-            my_money = my_money - item3;
-            
-        } else{
+            my_money = my_money - price;
+            weapon = 'normal'
+            document.querySelector('.game_money').innerHTML=`<img src="./etherium.png"> ${my_money}`;
+            console.log(my_money);
+        }else{
             let want = document.querySelector('.wanna_buy') 
-            want.innerHTML = "you don't have enough money!!<button class='ok2'>Okay..</button>"
-            let btn = document.querySelector('.ok2');
-            btn.onclick = function(){
-                popup.classList.remove('is_active');
-            }
+                want.innerHTML = "you don't have enough money!!<button class='ok2'>Okay..</button>"
+                let btn = document.querySelector('.ok2');
+                btn.onclick = function(){
+                    popup.classList.remove('is_active');
+        }}
+
+        // } else if(my_money - item2 >= 0){
+        //     popup.classList.remove('is_active');
+        //     my_money = my_money - item2;
+           
+        // } else if(my_money - item3 >= 0){
+        //     popup.classList.remove('is_active');
+        //     my_money = my_money - item3;
+            
+        // } else{
+        //     let want = document.querySelector('.wanna_buy') 
+        //     want.innerHTML = "you don't have enough money!!<button class='ok2'>Okay..</button>"
+        //     let btn = document.querySelector('.ok2');
+        //     btn.onclick = function(){
+        //         popup.classList.remove('is_active');
+        //     }
+        // }
+    }
+
+    function nomalWeapon(params) {
+        if (my_money - weapon_norm >= 0) {
+            popup.classList.remove('is_active');
+            my_money = my_money - weapon_norm;
+            weapon = 'normal'
+            console.log(my_money);
+            document.querySelector('.game_money').innerHTML=`<img src="./etherium.png"> ${my_money}`;
         }
     }
+
 
 
 
@@ -79,8 +163,35 @@ document.querySelector('#item3').innerHTML = `<img src="./etherium.png">${item3}
 console.log(my_money);
 
 
+//토글 함수
+function toggle(a) {
+    
+   let weapon=document.querySelector('.weapon_item'); 
+   let armor=document.querySelector('.armor_item');
+   let life=document.querySelector('.life_item');
+    if (getComputedStyle(weapon).display==='none') {
+        weapon.style.display ='block';
+        armor.style.display='none';
+        life.style.display='none'
+    }else if (getComputedStyle(armor).display ==='none') {
+        armor.style.display ='block';
+        weapon.style.display='none';
+        life.style.display='none'
+        console.log(weapon.style.display);
+        console.log(getComputedStyle(weapon).display)
+        
+    }else if (getComputedStyle(life).display==='none') {
+        life.style.display ='block';
+        weapon.style.display='none';
+        armor.style.display='none'
+        console.log(life.style.display);
+        console.log(getComputedStyle(life).display)
+    }
+}
 
-
+function temp(params) {
+    console.log(params);
+}
 
 // function noMoney() {
 //     let noPopup = document.querySelector('.noMoney_background');
