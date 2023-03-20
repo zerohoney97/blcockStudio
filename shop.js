a43bd0b8dc16061b9a4d2d2a227bb7cbe03f13bd
1chori <pro4456@naver.com>
2023-03-20 14:17:08 +0900

    shop2

--- shop.js
+++ shop.js
@@ -11,9 +11,12 @@ let sword = new Item(100,0,0,0);
 let shield = new Item(0,80,20,0);
 let drink = new Item(0,0,120,70);
 
-let weapon = document.querySelector('.weapon .item_info');
-let armor = document.querySelector('.armor .item_info');
-let life = document.querySelector('.life .item_info');
+let weapon = document.querySelector('.normal .item_info');
+let armor = document.querySelector('.rare .item_info');
+let life = document.querySelector('.legend .item_info');
+
+
+
 
 weapon.innerHTML = 'ATK : ' +sword.ATK+ '&nbsp&nbsp'+ 'DEF : ' +sword.DEF+'\n'+'<br>HP : ' +sword.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +sword.MP;
 armor.innerHTML = 'ATK : ' +shield.ATK+ '&nbsp&nbsp'+ 'DEF : ' +shield.DEF+'\n'+'<br>HP : ' +shield.HP+'&nbsp&nbsp&nbsp'+ 'MP : ' +shield.MP;
@@ -31,23 +34,68 @@ function buy() {
         popup.classList.add('is_active');
     }
 }
-
 // 돈이 부족할 때 발생하는 event
 
-function noMoney() {
-    let noPopup = document.querySelector('.noMoney_background');
-    let my_money = parseInt(document.querySelector('.game_money'));
-    let items = parseInt(document.querySelectorAll('.item_price'));
- 
-    console.log(noPopup.className);
-    console.log(noPopup.classList);
+let my_money = 1500;
+let item1 = 1000;
+let item2 = 500;
+let item3 = 400;
 
-    if (noPopup.classList.contains('is_active') || my_money < items) {  // 팝업 비활성화
-        noPopup.classList.remove('is_active');  // 팝업 활성화
-    } else {
-        noPopup.classList.add('is_active'); // 팝업 비활성화
+document.querySelector('.game_money').innerHTML=`<img src="./etherium.png"> ${my_money}`;
+document.querySelector('#item1').innerHTML = `<img src="./etherium.png">${item1}`;
+document.querySelector('#item2').innerHTML = `<img src="./etherium.png">${item2}`;
+document.querySelector('#item3').innerHTML = `<img src="./etherium.png">${item3}`;
+
+
+// yes버튼 눌렀을 때 함수
+
+    function onclickYes() {
+        let popup = document.querySelector('.popup_background');
+        if (my_money - item1 >= 0) {
+            popup.classList.remove('is_active');
+            my_money = my_money - item1;
+           
+        } else if(my_money - item2 >= 0){
+            popup.classList.remove('is_active');
+            my_money = my_money - item2;
+           
+        } else if(my_money - item3 >= 0){
+            popup.classList.remove('is_active');
+            my_money = my_money - item3;
+            
+        } else{
+            let want = document.querySelector('.wanna_buy') 
+            want.innerHTML = "you don't have enough money!!<button class='ok2'>Okay..</button>"
+            let btn = document.querySelector('.ok2');
+            btn.onclick = function(){
+                popup.classList.remove('is_active');
+            }
+        }
     }
-}
+
+
+
+
+console.log(my_money);
+
+
+
+
+
+// function noMoney() {
+//     let noPopup = document.querySelector('.noMoney_background');
+//     let my_money = parseInt(document.querySelector('.game_money'));
+//     let items = parseInt(document.querySelectorAll('.item_price'));
+ 
+//     console.log(noPopup.className);
+//     console.log(noPopup.classList);
+
+//     if (noPopup.classList.contains('is_active') || my_money < items) {  // 팝업 비활성화
+//         noPopup.classList.remove('is_active');  // 팝업 활성화
+//     } else {
+//         noPopup.classList.add('is_active'); // 팝업 비활성화
+//     }
+// }
 
 
 
